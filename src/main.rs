6a12436974210d9ed255efde08f40bd36a590e9c
shikai liu <lsk569937453@gmail.com>
2024-06-25 11:08:37 +0800

    Second commit.

--- src/main.rs
+++ src/main.rs
@@ -14,13 +14,20 @@ const IPS: &'static [&str] = &[
 ];
 #[tokio::main]
 async fn main() {
+    if let Err(e) = main_with_error().await {
+        println!("{}", e);
+    }
+}
+async fn main_with_error() -> Result<(), anyhow::Error> {
     // std::env::set_var("RUST_LOG", "trace");
+
     env_logger::init();
     if let Ok(ip) = get_ip().await {
         println!("{}", ip);
+        db_maxmind(ip).await?;
     }
+    Ok(())
 }
-
 async fn get_ip() -> Result<String, anyhow::Error> {
     let local_addr = IpAddr::from([0, 0, 0, 0]);
 
@@ -51,4 +58,26 @@ async fn get_ip() -> Result<String, anyhow::Error> {
         }
     }
     Err(anyhow!("Can not get the ip!"))
+}
+async fn db_maxmind(ip: String) -> Result<(), anyhow::Error> {
+    let local_addr = IpAddr::from([0, 0, 0, 0]);
+
+    let client = reqwest::Client::builder()
+        .no_proxy()
+        .connection_verbose(true)
+        .local_address(local_addr)
+        .build()?;
+    let url = format!("https://ipinfo.check.place/{}?lang=zh", ip);
+    let resp = client
+        .get(url)
+        .header("Host", "ipinfo.check.place")
+        .header("User-Agent", "curl/8.4.0")
+        .send()
+        .await?;
+
+    if resp.status() == StatusCode::OK {
+        let text: String = resp.text().await?;
+        println!("{}", text);
+    }
+    Ok(())
 }
